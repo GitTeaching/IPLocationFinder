6eb7fc929494f6d39820a2ce7be9692fe1ac873f
hedia89 <mecool07@live.fr>
2016-10-11 14:17:44 +0100

    first commit

---
+++ src/org/soa/ws/tp1/IPLocationFinder.java
@@ -0,0 +1,23 @@
+package org.soa.ws.tp1;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import net.webservicex.GeoIPServiceSoap;
+
+public class IPLocationFinder {
+
+	public static void main(String[] args) {
+		if (args.length != 1)
+			System.out.println("Vous devez entrer une adress IP.");
+		else {
+			String ipAddress = args[0];
+			
+			GeoIPService geoIPService = new GeoIPService();
+			GeoIPServiceSoap geoIPServiceSoap = geoIPService.getGeoIPServiceSoap();
+			GeoIP geoIP = geoIPServiceSoap.getGeoIP(ipAddress);
+			System.out.println(geoIP.getCountryName());
+		}
+
+	}
+
+}
