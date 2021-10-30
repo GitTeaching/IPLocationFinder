49254c78aa81d6790e6271b52c0a1a0c3184e2cc
Aicha Aid <aid_aicha@hotmail.fr>
2021-10-30 11:15:08 +0100

    Update RandomImpl.java

--- src/org/soa/ws/tp3/RandomImpl.java
+++ src/org/soa/ws/tp3/RandomImpl.java
@@ -6,11 +6,13 @@ import javax.jws.WebService;
 public class RandomImpl implements Random{
 
 	@Override
+	@WebMethod
 	public double getRandomValue() {
 		return Math.random();
 	}
 
 	@Override
+	@WebMethod
 	public double getSinusValue(double val) {
 		return Math.sin(val);
 	}
